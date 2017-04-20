4746e99a718b08e083eac62f4718007a954bbba4
Olufunsho Olagundoye <Funsholag@gmail.com>
2017-04-20 17:54:40 -0400

    Yow

---
+++ Funsho/views/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener(“DOMContentLoaded”, function(){
+//     document.getElementById("myDropdown").classList.toggle("show");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
