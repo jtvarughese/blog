d400c9d6b7335a946859938f771c2a5e7a25f9dc
Joel Varughese <jtvarughese@gmail.com>
2017-04-20 11:06:48 -0400

    pages in view all linked and working

--- app.rb
+++ app.rb
@@ -7,18 +7,15 @@ set :database, "sqlite3:xhosa.sqlite3"
 get "/" do
   erb :home
 end
-#
-# get "/feed" do
-#   Feed erb
-#   erb :feed
-# end
-#
-# get "/users" do
-#   users
-#   erb :users
-# end
-#
-# get "my-account" do
-#   my account
-#   erb :myaccount
-# end
+
+get "/feed" do
+  erb :feed
+end
+
+get "/users" do
+  erb :users
+end
+
+get "/myaccount" do
+  erb :myaccount
+end
