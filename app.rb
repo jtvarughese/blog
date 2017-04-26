b0b7f300d66f4d55073717e85ca62f6a14bfefe8
jtvarughese <jtvarughese@gmail.com>
2017-04-25 20:19:55 -0400

    new changes that work for users, myaccount, feed

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@ require "./models"
 # this is needed for sinatra flash messaging
 # require "bundle/setup"
 require "sinatra/flash"
+require 'pp'
 
 
-set :database, "sqlite3:xhosa.sqlite3"
+set :database, "sqlite3:new.sqlite3"
 
 set :sessions, true
 
@@ -14,13 +15,33 @@ get "/" do
   erb :home
 end
 
+# Instance variable post, shows recent posts
 get "/feed" do
+  # this grabs all the user's posts
+  @posts = Post.all
+  # this shows the recent post
+  @latest_post = Post.all.last(10)
   erb :feed
+end
+
+post '/feed' do
+  @post =  Post.create(params[:post])
+  session[:user_id] = @post.id
+  session[:user_id] = @user.id
+  flash[:notice] = "You created a new post!"
+  redirect '/feed'
+end
+
+# this is what creates the posts
+post '/create-post' do
+  flash[:notice] = "You created a new post!"
+  redirect '/feed'
 end
 
 get "/users" do
   @users = User.all
-  erb :users
+  pp @users
+  erb :index
 end
 
 get "/users/:id" do
@@ -30,7 +51,18 @@ end
 
 get "/myaccount" do
   @user = User.find(session[:user_id])
+  pp @user
   erb :myaccount
+end
+
+get '/user-profile' do
+  @user_profile = Profile.find_by(user_id:session[:user_id])
+  erb :myaccount
+end
+
+get "/profile/:id" do
+  @user_profile = Profile.find(params[:id])
+  erb :user_profile
 end
 
 get "/signin" do
@@ -44,14 +76,14 @@ end
 # this creates new user
 post '/join' do
   @user = User.create(
-  # fname: params[:fname],
-  # lname: params[:lname],
+  fname: params[:fname],
+  lname: params[:lname],
   username: params[:username],
-  password: params[:password]
+  password: params[:password],
   )
   session[:user_id] = @user.id
   flash[:notice] = "You created a new account!"
-  redirect '/join'
+  redirect '/'
 end
 
 # this is what lets the user sign in
@@ -60,7 +92,7 @@ post '/signin' do
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
     flash[:notice] = "You have signed in successfully."
-    redirect '/'
+    redirect '/signin'
   else
     flash[:notice] = "Your username and password do not match."
   end
@@ -70,6 +102,17 @@ post '/signin' do
 # goes to home page on sign out
 get "/signout" do
   session[:user_id] = nil
+  redirect "/"
+end
+
+post "/signout" do
+  session[:user_id] = nil
+  redirect "/"
+end
+
+post "/delete-account" do
+  User.find(session[:user_id]).destroy
+    session[:user_id] = nil
   redirect "/"
 end
 
